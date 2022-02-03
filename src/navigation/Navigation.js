f492a1b250686a97c257db9b5a7adc03a4b9f59b
jjestrada2 <juan5801331@gmail.com>
2022-02-03 13:35:10 -0800

    publico proyeto desde la terminal

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import FavoriteNavigation from "./FavoriteNavigation";
+import PokedexNavigation from "./PokedexNavigation";
+import AccountNavigation from "./AccountNavigation";
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteNavigation}
+        options={{
+        
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Pokedex"
+        component={PokedexNavigation}
+        options={{
+          
+          tabBarIcon: () => renderPokeball(),
+        }}
+      />
+
+      <Tab.Screen
+        name="Account"
+        component={AccountNavigation}
+        options={{
+          
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require("../assets/pokemonBall.png")}
+      style={{ width: 75, height: 75, top: -15 }}
+    />
+  );
+}
